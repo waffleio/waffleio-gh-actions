13509b329dd133fd72608095470ba2d735c1b2e2
Adam Zolyak <adam.zolyak@gmail.com>
2018-12-11 15:09:02 -0700

    add debugging for testing

--- action-bulklabels/app.js
+++ action-bulklabels/app.js
@@ -1,69 +1,75 @@
-console.log("started nodejs...");
+console.log('started nodejs...')
 
-const helpers = require("./helpers");
+const helpers = require('./helpers')
 
 //require octokit rest.js
 //more info at https://github.com/octokit/rest.js
-const octokit = require("@octokit/rest")();
+const octokit = require('@octokit/rest')()
 
 //set octokit auth to action's GITHUB_TOKEN env variable
 octokit.authenticate({
-  type: "app",
+  type: 'app',
   token: process.env.GITHUB_TOKEN
-});
+})
 
 //set eventOwner and eventRepo based on action's env variables
-const eventOwnerAndRepo = process.env.GITHUB_REPOSITORY;
-const eventOwner = helpers.getOwner(eventOwnerAndRepo);
-const eventRepo = helpers.getRepo(eventOwnerAndRepo);
+const eventOwnerAndRepo = process.env.GITHUB_REPOSITORY
+const eventOwner = helpers.getOwner(eventOwnerAndRepo)
+const eventRepo = helpers.getRepo(eventOwnerAndRepo)
 
 async function bulkLabelAdd() {
   //read contents of action's event.json
   const eventData = await helpers.readFilePromise(
-    ".." + process.env.GITHUB_EVENT_PATH
-  );
-  const eventJSON = JSON.parse(eventData);
+    '..' + process.env.GITHUB_EVENT_PATH
+  )
+  const eventJSON = JSON.parse(eventData)
 
   //set eventAction and eventIssueNumber
-  eventAction = eventJSON.action;
-  eventIssueNumber = eventJSON.issue.number;
-  eventIssueBody = eventJSON.issue.body;
+  eventAction = eventJSON.action
+  eventIssueNumber = eventJSON.issue.number
+  eventIssueBody = eventJSON.issue.body
 
   //if an issue was opened, edited, or reopened
-  if (eventAction === "opened") {
+  if (eventAction === 'opened') {
     //check if there are bulk labels
-    const bulkLabels = helpers.getBulkLabels(eventIssueBody);
+    const bulkLabels = helpers.getBulkLabels(eventIssueBody)
 
     //if one or more bulk labels
     if (bulkLabels) {
+      console.log('bulk labels found in issue...')
+
       const repoLabels = await helpers.getRepoLabels(
         octokit,
         eventOwner,
         eventRepo
-      );
+      )
 
       const repoShortLabels = await Promise.all(
         repoLabels.map(helpers.getShortLabelName)
-      );
+      )
 
       for (const issueLabel of bulkLabels) {
         for (const repoLabel of repoShortLabels) {
           if (issueLabel === repoLabel.shortName) {
+            console.log('issue label matches repo issue; labeling...')
+
             helpers.addLabel(
               octokit,
               eventOwner,
               eventRepo,
               eventIssueNumber,
               repoLabel.name
-            );
+            )
           }
         }
       }
+    } else {
+      console.log('NO bulk labels found in issue...')
     }
   }
 }
 
 //run the function
-bulkLabelAdd();
+bulkLabelAdd()
 
-module.exports.bulkLabelAdd = bulkLabelAdd;
+module.exports.bulkLabelAdd = bulkLabelAdd
